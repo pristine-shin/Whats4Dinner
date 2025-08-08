8398e5b3e565a5828801bf03b457328230f8d98d
Pristine Shin <prisvsyo@gmail.com>
2025-08-08 13:24:09 -0700

    first commit - set up react vite template, installed tailwind, started home page component

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className='text-amber-300'>What's for Dinner?</h1>
+    </div>
+  )
+}
+
+export default HomePage
